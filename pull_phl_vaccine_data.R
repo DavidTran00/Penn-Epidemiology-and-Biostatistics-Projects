c702d70ef7fda7cacf2b39641fc37a9f7258a239
DavidTran00 <90878585+DavidTran00@users.noreply.github.com>
2021-09-16 21:00:54 -0400

    Create pull_phl_vaccine_data.R

---
+++ pull_phl_vaccine_data.R
@@ -0,0 +1,40 @@
+######################################
+######################################
+# Pulls COVID vaccine data for
+# Philadelphia based on zip code
+
+# Set to run daily using cron job
+# to create time series data set
+######################################
+######################################
+
+#####################
+# load libraries
+#####################
+
+# remotes::install_github("CityOfPhiladelphia/rphl")
+library(rphl)
+library(tidyverse)
+library(zip)
+
+#######################################
+# pull PHL vaccine data using Carto API
+#######################################
+
+phl_vacc <- get_carto(query = "SELECT * FROM covid_vaccines_by_zip", 
+                      format = "csv",
+                      base_url = "https://phl.carto.com/api/v2/sql", 
+                      stringsAsFactors = F)
+
+#######################################
+# export and zip files
+#######################################
+
+write_csv(phl_vacc, paste("./raw_phl_vacc_data/phl_vacc_", Sys.Date(),
+                          ".csv", sep = ""))
+
+files2zip <- list.files(path = "./raw_phl_vacc_data", pattern = "csv", full.names = TRUE)
+
+zip(zipfile = "phl_vacc_daily_data.zip", files = files2zip, root = ".",
+    mode = "cherry-pick")
+
